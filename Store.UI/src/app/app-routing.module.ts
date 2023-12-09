64e68a7b4033ee4b132bcc2297d227f545ec531d
Henrique Gouvea <henriquegouvea90@gmail.com>
2023-12-09 18:59:52 +0000

    Configure routes

--- Store.UI/src/app/app-routing.module.ts
+++ Store.UI/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from './product/list/list.component';
+import { InsertComponent } from './product/insert/insert.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: 'products', component: ListComponent },
+  { path: 'insert', component: InsertComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Store.UI/src/app/product/list/list.component.ts
+++ Store.UI/src/app/product/list/list.component.ts
@@ -15,19 +15,6 @@ export class ListComponent implements OnInit {
   ngOnInit(): void {
     this.productService.get().subscribe((data: Product[]) => {
       this.products = data;
-      console.log('data: ' + data);
-      console.log('this.products: ' + this.products);
     });
-
-    const x = this.products.length;
-    console.log('this.products2: ' + this.products);
-  }
-
-  filterProducts() : void {
-    
-  }
-
-  showCreatePage() {
-
   }
 }
